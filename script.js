be7c0db1384837d974e50d80604428573fe054d3
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-09-03 15:38:01 +0200

    end

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-console.log('JS OK');
+const { createApp } = Vue
 
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!',
+            image: 'https://d585tldpucybw.cloudfront.net/sfimages/default-source/default-album/870x220_telerik-blogsce2fdf065dfe46a2b2054ccc57d2d18d.png?sfvrsn=222ff053_1',
+        }
+    }
+}).mount('#app')
